65c558644aae626cbe06c35a12cd24de953668d6
bordes <bordes.yohan@gmail.com>
2021-03-23 01:04:24 +0100

    jsp

--- debug/moc_ConsoleTabWidget.cpp
+++ debug/moc_ConsoleTabWidget.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_ConsoleTabWidget_t {
-    QByteArrayData data[6];
-    char stringdata0[61];
+    QByteArrayData data[8];
+    char stringdata0[75];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,12 +35,14 @@ QT_MOC_LITERAL(0, 0, 16), // "ConsoleTabWidget"
 QT_MOC_LITERAL(1, 17, 13), // "removeConsole"
 QT_MOC_LITERAL(2, 31, 0), // ""
 QT_MOC_LITERAL(3, 32, 5), // "index"
-QT_MOC_LITERAL(4, 38, 11), // "openMenuAdd"
-QT_MOC_LITERAL(5, 50, 10) // "addConsole"
+QT_MOC_LITERAL(4, 38, 8), // "compiler"
+QT_MOC_LITERAL(5, 47, 4), // "exec"
+QT_MOC_LITERAL(6, 52, 11), // "openMenuAdd"
+QT_MOC_LITERAL(7, 64, 10) // "addConsole"
 
     },
     "ConsoleTabWidget\0removeConsole\0\0index\0"
-    "openMenuAdd\0addConsole"
+    "compiler\0exec\0openMenuAdd\0addConsole"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,7 +52,7 @@ static const uint qt_meta_data_ConsoleTabWidget[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -58,13 +60,17 @@ static const uint qt_meta_data_ConsoleTabWidget[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   34,    2, 0x0a /* Public */,
-       1,    0,   37,    2, 0x2a /* Public | MethodCloned */,
-       4,    0,   38,    2, 0x08 /* Private */,
-       5,    0,   39,    2, 0x08 /* Private */,
+       1,    1,   44,    2, 0x0a /* Public */,
+       1,    0,   47,    2, 0x2a /* Public | MethodCloned */,
+       4,    0,   48,    2, 0x0a /* Public */,
+       5,    0,   49,    2, 0x0a /* Public */,
+       6,    0,   50,    2, 0x08 /* Private */,
+       7,    0,   51,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -80,8 +86,10 @@ void ConsoleTabWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int
         switch (_id) {
         case 0: _t->removeConsole((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 1: _t->removeConsole(); break;
-        case 2: _t->openMenuAdd(); break;
-        case 3: _t->addConsole(); break;
+        case 2: _t->compiler(); break;
+        case 3: _t->exec(); break;
+        case 4: _t->openMenuAdd(); break;
+        case 5: _t->addConsole(); break;
         default: ;
         }
     }
@@ -116,13 +124,13 @@ int ConsoleTabWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 6;
     }
     return _id;
 }
